425231237be640d0a07e1d47b1392831f43da885
EfeAkhigbe <73133486+EfeAkhigbe@users.noreply.github.com>
2021-04-10 18:58:46 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector("div.hamburger");
+const navMenu = document.querySelector("ul.nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll("a.nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
